1db689c7e12d80be670a77ba69931ec35ef0f941
Nathanael Mueller <nathanaelmueller@ucrwpa-2-6-10-25-55-11.wnet.ucr.edu>
2018-05-31 16:35:11 -0700

    first commit

---
+++ TouchScreenButton.gd
@@ -0,0 +1,17 @@
+extends TouchScreenButton
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+func _on_TouchScreenButton_released():
+	get_tree().change_scene("res://game_scene_1.tscn")
